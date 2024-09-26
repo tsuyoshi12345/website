bdd44ed4486b97d146ddf1ac6114f3989b1d9530
zaka <samutosaku0612@icloud.com>
2024-09-26 18:45:13 +0900

    クラブ紹介ページ仮作成

--- frontend/react-project/src/pages/about.tsx
+++ frontend/react-project/src/pages/about.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import './css/About.css';
 
 const About: React.FC = () => {
   return (
-    <div>
-      <h1>クラブ紹介</h1>
-      <p>ドローンクラブの紹介文をここに追加します。</p>
+    <div className="container">
+      <div className="item image1">
+        <img src="apple.png" alt="画像1" />
+      </div>
+      <div className="item desc1">
+        <div className="description">
+          <h2>画像1の説明</h2>
+          <p>ここに画像1の説明文を入力します。これはサンプルのテキストです。</p>
+        </div>
+      </div>
+      <div className="item desc2">
+        <div className="description">
+          <h2>画像2の説明</h2>
+          <p>ここに画像2の説明文を入力します。これはサンプルのテキストです。</p>
+        </div>
+      </div>
+      <div className="item image2">
+        <img src="apple.png" alt="画像2" />
+      </div>
+      <div className="item image3">
+        <img src="apple.png" alt="画像3" />
+      </div>
+      <div className="item desc3">
+        <div className="description">
+          <h2>画像3の説明</h2>
+          <p>ここに画像3の説明文を入力します。これはサンプルのテキストです。</p>
+        </div>
+      </div>
     </div>
   );
 };
